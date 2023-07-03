c831e212efaff02a813f74274e44140b77d8ae40
German Kuber <german.kuber@outlook.com>
2023-07-03 22:13:37 +0200

    Add key reader

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+#![warn(clippy::all, clippy::pedantic)]
+mod editor;
+use editor::Editor;
+mod terminal;
+pub use editor::Position;
+pub use terminal::Terminal;
+fn main() {
+    Editor::default().run();
+}
