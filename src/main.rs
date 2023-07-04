717efce82ac34c1fffe6460464cedd103837c207
German Kuber <german.kuber@outlook.com>
2023-07-04 10:57:33 +0200

    Add cursos movment

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 #![warn(clippy::all, clippy::pedantic)]
 mod editor;
 use editor::Editor;
+mod document;
+mod row;
 mod terminal;
+pub use document::Document;
 pub use editor::Position;
+pub use row::Row;
 pub use terminal::Terminal;
 fn main() {
     Editor::default().run();
